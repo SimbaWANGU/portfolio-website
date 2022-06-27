8b0f485e9502546a6b488e08931121e27625210a
SimbaWANGU <simbawangulu@gmail.com>
2022-06-27 22:51:10 +0300

    Add more items to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,12 @@ import { NavLink, Routes, Route } from 'react-router-dom';
 import '../styles/navbar.scss';
 import HomeIcon from '@mui/icons-material/Home';
 import AssignmentIcon from '@mui/icons-material/Assignment';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 import ContactPageRoundedIcon from '@mui/icons-material/ContactPageRounded';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import ListAltIcon from '@mui/icons-material/ListAlt';
 import Me from '../assets/logo.png';
 
 function Navbar() {
@@ -11,7 +16,7 @@ function Navbar() {
     <>
       <div className="nav">
         <img src={Me} alt="myImage" />
-        <ul>
+        <ul className="linksList">
           <li>
             <NavLink
               to="/"
@@ -19,6 +24,15 @@ function Navbar() {
             >
               <HomeIcon className="navIcon" />
               <p>Home</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+            >
+              <AssignmentIndIcon className="navIcon" />
+              <p>About</p>
             </NavLink>
           </li>
           <li>
@@ -38,6 +52,28 @@ function Navbar() {
               <ContactPageRoundedIcon className="navIcon" />
               <p>Contact</p>
             </NavLink>
+          </li>
+        </ul>
+        <ul className="contactsList">
+          <li>
+            <a href="https://twitter.com/simb_erella">
+              <TwitterIcon />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/SimbaWANGU">
+              <GitHubIcon />
+            </a>
+          </li>
+          <li>
+            <a href="https://linkedin.com/in/simba-wangulu/">
+              <LinkedInIcon />
+            </a>
+          </li>
+          <li>
+            <a href="nkedin.com/in/simba-wangulu/">
+              <ListAltIcon />
+            </a>
           </li>
         </ul>
         <Routes>
