9a661eb53606b5278cea2bd688518770a32e7828
SimbaWANGU <simbawangulu@gmail.com>
2022-06-28 12:22:40 +0300

    Add tooltip to navbar contact icons

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import '../styles/landing.scss';
 
 function Landing() {
@@ -9,7 +10,9 @@ function Landing() {
         <p>Hi, my name is</p>
         <h1>Simba</h1>
         <h6>I’m a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me. </h6>
-        <button type="button">See Projects</button>
+        <NavLink to="/projects">
+          <button type="button">See Projects</button>
+        </NavLink>
       </div>
     </div>
   );
