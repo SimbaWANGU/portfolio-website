ee4d6bfc22450e641a40c4c3c78f8499b82b413b
SimbaWANGU <simbawangulu@gmail.com>
2022-06-29 17:53:55 +0300

    Add project src code and deployment links

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../styles/projects.scss';
 import ListSharpIcon from '@mui/icons-material/ListSharp';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
 import LinkIcon from '@mui/icons-material/Link';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
@@ -11,21 +14,21 @@ function Projects() {
         <div className="container animate glow delay-1">
           <div className="card">
             <div className="imgBx">
-              <ListSharpIcon className="projectIcon" style={{ color: '#ffffff' }} />
+              <SportsSoccerIcon className="projectIcon" style={{ color: '#ffffff' }} />
             </div>
             <div className="contentBx">
-              <h2>Todo List</h2>
+              <h2>Football Hub</h2>
               <div className="technology">
                 <h3>Technology:</h3>
-                <span>HTML</span>
-                <span>JS</span>
-                <span>CSS</span>
+                <span>React</span>
+                <span>Redux</span>
+                <span>SCSS</span>
               </div>
               <div className="links">
-                <a href="/">
+                <a href="https://foot-y.herokuapp.com/">
                   <LinkIcon className="linkIcons" />
                 </a>
-                <a href="/">
+                <a href="https://github.com/SimbaWANGU/capstone3">
                   <GitHubIcon className="linkIcons" />
                 </a>
               </div>
@@ -35,22 +38,21 @@ function Projects() {
         <div className="container animate glow delay-3">
           <div className="card">
             <div className="imgBx">
-              <ListSharpIcon className="projectIcon" style={{ color: '#ffffff' }} />
+              <MenuBookIcon className="projectIcon" style={{ color: '#ffffff' }} />
             </div>
             <div className="contentBx">
-              <h2>Todo List</h2>
+              <h2>Bookstore</h2>
               <div className="technology">
                 <h3>Technology:</h3>
-                <span>HTML</span>
-                <span>JS</span>
-                <span>CSS</span>
-                <span>10</span>
+                <span>React</span>
+                <span>Redux</span>
+                <span>SCSS</span>
               </div>
               <div className="links">
-                <a href="/">
+                <a href="https://bookstor-e.herokuapp.com/">
                   <LinkIcon className="linkIcons" />
                 </a>
-                <a href="/">
+                <a href="https://github.com/SimbaWANGU/bookstore">
                   <GitHubIcon className="linkIcons" />
                 </a>
               </div>
@@ -60,22 +62,21 @@ function Projects() {
         <div className="container animate glow delay-5">
           <div className="card">
             <div className="imgBx">
-              <ListSharpIcon className="projectIcon" style={{ color: '#ffffff' }} />
+              <TipsAndUpdatesIcon className="projectIcon" style={{ color: '#ffffff' }} />
             </div>
             <div className="contentBx">
-              <h2>Todo List</h2>
+              <h2>Kenyan Creatives</h2>
               <div className="technology">
                 <h3>Technology:</h3>
                 <span>HTML</span>
-                <span>JS</span>
                 <span>CSS</span>
-                <span>10</span>
+                <span>JS</span>
               </div>
               <div className="links">
-                <a href="/">
+                <a href="https://simbawangu.github.io/Kenyan-Creative---Microverse-Capstone-Project/public/index.html">
                   <LinkIcon className="linkIcons" />
                 </a>
-                <a href="/">
+                <a href="https://github.com/SimbaWANGU/Kenyan-Creative---Microverse-Capstone-Project">
                   <GitHubIcon className="linkIcons" />
                 </a>
               </div>
@@ -91,16 +92,15 @@ function Projects() {
               <h2>Todo List</h2>
               <div className="technology">
                 <h3>Technology:</h3>
-                <span>HTML</span>
+                <span>React</span>
+                <span>SCSS</span>
                 <span>JS</span>
-                <span>CSS</span>
-                <span>10</span>
               </div>
               <div className="links">
-                <a href="/">
+                <a href="https://t-odo.herokuapp.com/">
                   <LinkIcon className="linkIcons" />
                 </a>
-                <a href="/">
+                <a href="https://github.com/SimbaWANGU/react-todo">
                   <GitHubIcon className="linkIcons" />
                 </a>
               </div>
