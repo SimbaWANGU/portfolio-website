7cd7e37109c7ce200967963bd73f3499abc6f6d7
SimbaWANGU <simbawangulu@gmail.com>
2022-06-29 13:19:53 +0300

    Add project card design

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import '../styles/projects.scss';
+import ListSharpIcon from '@mui/icons-material/ListSharp';
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className="projects">
+      <div className="container">
+        <div className="card">
+          <div className="imgBx">
+            <ListSharpIcon style={{ color: '#ffffff' }} />
+          </div>
+          <div className="contentBx">
+            <h2>Nike Shoes</h2>
+            <div className="size">
+              <h3>Size :</h3>
+              <span>7</span>
+              <span>8</span>
+              <span>9</span>
+              <span>10</span>
+            </div>
+            <div className="color">
+              <h3>Color :</h3>
+              <span />
+              <span />
+              <span />
+            </div>
+            <a href="/">Buy Now</a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
