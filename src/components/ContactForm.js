33a33220322e90677b90f642a117699fc739dd27
SimbaWANGU <simbawangulu@gmail.com>
2022-07-01 12:57:59 +0300

    Ensure form works as expected

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,10 +1,14 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import toast, { Toaster } from 'react-hot-toast';
 
 const ContactForm = () => {
   const form = useRef();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
   const SERVICE_ID = 'service_vjo5wik';
   const TEMPLATE_ID = 'template_91zrc2g';
@@ -16,25 +20,49 @@ const ContactForm = () => {
     toast.promise(
       emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY), {
         loading: 'Saving...',
-        success: <b>Settings saved!</b>,
-        error: <b>Could not save.</b>,
+        success: <b>Message Sent</b>,
+        error: <b>Please try again</b>,
       },
     );
+
+    setName('');
+    setEmail('');
+    setMessage('');
   };
 
   return (
     <div className="formDiv">
       <Toaster />
       <form ref={form} onSubmit={sendEmail} className="form">
-        <input type="text" name="user_name" placeholder="Name" className="nameInput" />
+        <input
+          type="text"
+          name="user_name"
+          placeholder="Name"
+          className="nameInput"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <span className="nameInputLabel">
           <p>Name</p>
         </span>
-        <input type="email" name="user_email" placeholder="Email" className="emailInput" />
+        <input
+          type="email"
+          name="user_email"
+          placeholder="Email"
+          className="emailInput"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <span className="emailInputLabel">
           <p>Email</p>
         </span>
-        <textarea name="message" placeholder="Message" className="messageInput" />
+        <textarea
+          name="message"
+          placeholder="Message"
+          className="messageInput"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
         <span className="messageInputLabel">
           <p>Message</p>
         </span>
